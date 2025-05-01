459795e96ed834030cfcbd2bf53f2977fd2bf6a9
Camilo Gil <giljuancamilo39@gmail.com>
2025-05-01 01:24:58 -0500

    Clase Pokemon modificada para usar se en el gui con las nuevas caracteristicas

--- src/pokemons/ElectricPokemon.java
+++ src/pokemons/ElectricPokemon.java
@@ -9,36 +9,42 @@ public class ElectricPokemon extends Pokemon {
 
 
     //Constructor and somes attacks to the type of pokemon
-    public ElectricPokemon(String name, Type type, double hp, double defense) {
-        super(name, type, hp, defense);
-        attacksOfClass.add(new Attack("Impactrueno", Type.ELECTRIC, 35.0));
-        attacksOfClass.add(new Attack("Trueno", Type.ELECTRIC, 60.0));
-        attacksOfClass.add(new Attack("Rayo", Type.ELECTRIC, 55.0));
-        attacksOfClass.add(new Attack("Chispa", Type.ELECTRIC, 40.0));
-        attacksOfClass.add(new Attack("Electrocañón", Type.ELECTRIC, 65.0));
-        attacksOfClass.add(new Attack("Puño Trueno", Type.ELECTRIC, 50.0));
-        attacksOfClass.add(new Attack("Voltio Cruel", Type.ELECTRIC, 60.0));
-        attacksOfClass.add(new Attack("Onda Trueno", Type.ELECTRIC, 20.0));
-        attacksOfClass.add(new Attack("Electropatada", Type.ELECTRIC, 45.0));
-        attacksOfClass.add(new Attack("Carga", Type.ELECTRIC, 10.0));
-        attacksOfClass.add(new Attack("Voltiocambio", Type.ELECTRIC, 50.0));
-        attacksOfClass.add(new Attack("Chispa Salvaje", Type.ELECTRIC, 55.0));
-        attacksOfClass.add(new Attack("Electropulso", Type.ELECTRIC, 35.0));
-        attacksOfClass.add(new Attack("Campo Eléctrico", Type.ELECTRIC, 0.0));
-        attacksOfClass.add(new Attack("Electroshock", Type.ELECTRIC, 40.0));
+    public ElectricPokemon(String name, Type type, short hp, short defenseFisic, short defenseSpecial, short speed) {
+        super(name, type, hp, defenseFisic, defenseSpecial, speed);
+
+        //Add fisic attacks to attacks of class
+
+        attacksOfClass.add(new Attack("Puño Eléctrico", Type.ELECTRIC, (short) 50, false));
+        attacksOfClass.add(new Attack("Rayo Corto", Type.ELECTRIC, (short) 45, false));
+        attacksOfClass.add(new Attack("Impacto Voltaje", Type.ELECTRIC, (short) 60, false));
+        attacksOfClass.add(new Attack("Tacleo Eléctrico", Type.ELECTRIC, (short) 40, false));
+        attacksOfClass.add(new Attack("Golpe Relámpago", Type.ELECTRIC, (short) 55, false));
+        attacksOfClass.add(new Attack("Puño Trueno", Type.ELECTRIC, (short) 50, false));
+        attacksOfClass.add(new Attack("Voltio", Type.ELECTRIC, (short) 60, false));
+        attacksOfClass.add(new Attack("Choque Eléctrico", Type.ELECTRIC, (short) 65, false));
+        attacksOfClass.add(new Attack("Estallido Voltaico", Type.ELECTRIC, (short) 70, false));
+        attacksOfClass.add(new Attack("Rayo Impacto", Type.ELECTRIC, (short) 75, false));
+
+        //Add specials attacks to attacks of class
+
+        attacksOfClass.add(new Attack("Rayo", Type.ELECTRIC, (short) 80, true));
+        attacksOfClass.add(new Attack("Trueno", Type.ELECTRIC, (short) 90, true));
+        attacksOfClass.add(new Attack("Relámpago", Type.ELECTRIC, (short) 75, true));
+        attacksOfClass.add(new Attack("Chispa Eléctrica", Type.ELECTRIC, (short) 60, true));
+        attacksOfClass.add(new Attack("Electrocañón", Type.ELECTRIC, (short) 85, true));
+        attacksOfClass.add(new Attack("Carga Eléctrica", Type.ELECTRIC, (short) 70, true));
+        attacksOfClass.add(new Attack("Destello", Type.ELECTRIC, (short) 65, true));
+        attacksOfClass.add(new Attack("Electrochorro", Type.ELECTRIC, (short) 80, true));
+        attacksOfClass.add(new Attack("Tornado Eléctrico", Type.ELECTRIC, (short) 90, true));
+        attacksOfClass.add(new Attack("Rayo Solar Eléctrico", Type.ELECTRIC, (short) 75, true));
+
     }
 
     @Override
-    public void doAttack(Pokemon oponentPokemon, Scanner scanner) {
-        System.out.println("\t\tAtaques");
-        for (int i = 0; i < MAXATTACKS; i++) {
-            System.out.println((i + 1) + ". " + attackSelected.get(i));
-        }
+    public void doAttack(Pokemon oponentPokemon, byte indexAttackSelected) {
 
-
-        int indexAttackSelected;
-        indexAttackSelected = scanner.nextInt();
         double effectivity;
+
         switch (oponentPokemon.getType()){
             case WATER -> effectivity = 2.0;
             case ROCK -> effectivity = 2.0;
@@ -46,6 +52,16 @@ public class ElectricPokemon extends Pokemon {
             default -> effectivity = 1.0;
         }
 
-        oponentPokemon.receiveDamage(Math.abs((attackSelected.get(indexAttackSelected-1).getAttackPower() - oponentPokemon.getDefense())*effectivity));
+        Attack selectedAttack = attacksInstance.get(indexAttackSelected);
+
+        double damage;
+
+        if (selectedAttack.isSpecial()) {
+            damage = ((selectedAttack.getAttackPower()) / (double) oponentPokemon.getDefenseSpecial()) + 2;
+        } else {
+            damage = ((selectedAttack.getAttackPower()) / (double) oponentPokemon.getDefenseFisic()) + 2;
+        }
+
+        oponentPokemon.receiveDamage((short)(damage*effectivity));
     }
 }
--- src/pokemons/GroundPokemon.java
+++ src/pokemons/GroundPokemon.java
@@ -8,34 +8,41 @@ import src.actions.Attack;
 public class GroundPokemon extends Pokemon {
 
     //Constructor and somes attacks to the type of pokemon
-    public GroundPokemon(String name, Type type, double hp, double defense) {
-        super(name, type, hp, defense);
-        attacksOfClass.add(new Attack("Terremoto", Type.GROUND, 60.0));
-        attacksOfClass.add(new Attack("Excavar", Type.GROUND, 50.0));
-        attacksOfClass.add(new Attack("Fuerza Tierra", Type.GROUND, 55.0));
-        attacksOfClass.add(new Attack("Disparo Lodo", Type.GROUND, 30.0));
-        attacksOfClass.add(new Attack("Bofetón Lodo", Type.GROUND, 35.0));
-        attacksOfClass.add(new Attack("Magnitud", Type.GROUND, 45.0));
-        attacksOfClass.add(new Attack("Ataque Arena", Type.GROUND, 25.0));
-        attacksOfClass.add(new Attack("Cañón de Tierra", Type.GROUND, 40.0));
-        attacksOfClass.add(new Attack("Hundimiento", Type.GROUND, 60.0));
-        attacksOfClass.add(new Attack("Terratemblor", Type.GROUND, 50.0));
-        attacksOfClass.add(new Attack("Golpe Arena", Type.GROUND, 20.0));
-        attacksOfClass.add(new Attack("Barrida", Type.GROUND, 35.0));
-        attacksOfClass.add(new Attack("Lodo", Type.GROUND, 30.0));
-        attacksOfClass.add(new Attack("Trampa Rocas", Type.GROUND, 40.0));
-        attacksOfClass.add(new Attack("Arena Ardiente", Type.GROUND, 40.0));
+    public GroundPokemon(String name, Type type, short hp, short defenseFisic, short defenseSpecial, short speed) {
+        super(name, type, hp, defenseFisic, defenseSpecial, speed);
+
+        //Add fisic attacks to attacks of class
+
+        attacksOfClass.add(new Attack("Terremoto", Type.GROUND, (short) 80, false));
+        attacksOfClass.add(new Attack("Golpe de Arena", Type.GROUND, (short) 50, false));
+        attacksOfClass.add(new Attack("Pisotón Terrestre", Type.GROUND, (short) 55, false));
+        attacksOfClass.add(new Attack("Corte Rocoso", Type.GROUND, (short) 60, false));
+        attacksOfClass.add(new Attack("Bucle Subterráneo", Type.GROUND, (short) 65, false));
+        attacksOfClass.add(new Attack("Puño Tierra", Type.GROUND, (short) 70, false));
+        attacksOfClass.add(new Attack("Impacto de Tierra", Type.GROUND, (short) 75, false));
+        attacksOfClass.add(new Attack("Desgarro Subterráneo", Type.GROUND, (short) 85, false));
+        attacksOfClass.add(new Attack("Tajo Subterráneo", Type.GROUND, (short) 55, false));
+        attacksOfClass.add(new Attack("Sacudida Terrestre", Type.GROUND, (short) 60, false));
+
+        //Add specials attacks to attacks of class
+
+        attacksOfClass.add(new Attack("Roca Filosa", Type.GROUND, (short) 70, true));
+        attacksOfClass.add(new Attack("Sismo", Type.GROUND, (short) 85, true));
+        attacksOfClass.add(new Attack("Terremoto Infernal", Type.GROUND, (short) 95, true));
+        attacksOfClass.add(new Attack("Cañón de Arena", Type.GROUND, (short) 80, true));
+        attacksOfClass.add(new Attack("Tsunami Subterráneo", Type.GROUND, (short) 90, true));
+        attacksOfClass.add(new Attack("Terraforma", Type.GROUND, (short) 75, true));
+        attacksOfClass.add(new Attack("Bomba Lodo", Type.GROUND, (short) 80, true));
+        attacksOfClass.add(new Attack("Puño Rocoso", Type.GROUND, (short) 70, true));
+        attacksOfClass.add(new Attack("Torbellino de Tierra", Type.GROUND, (short) 85, true));
+        attacksOfClass.add(new Attack("Rugido de la Tierra", Type.GROUND, (short) 75, true));
+
 
     }
 
     @Override
-    public void doAttack(Pokemon oponentPokemon, Scanner scanner) {
-        System.out.println("\t\tAtaques");
-        for (int i = 0; i < MAXATTACKS; i++) {
-            System.out.println((i + 1) + ". " + attackSelected.get(i));
-        }
+    public void doAttack(Pokemon oponentPokemon, byte indexAttackSelected) {
 
-        int indexAttackSelected = scanner.nextInt();
         double effectivity;
         switch (oponentPokemon.getType()){
             case ELECTRIC -> effectivity = 2.0;
@@ -44,6 +51,16 @@ public class GroundPokemon extends Pokemon {
             case PLANT -> effectivity = 5.0;
             default -> effectivity = 1.0;
         }
-        oponentPokemon.receiveDamage((Math.abs((attackSelected.get(indexAttackSelected-1).getAttackPower() - oponentPokemon.getDefense())*effectivity)));;
+        Attack selectedAttack = attacksInstance.get(indexAttackSelected);
+
+        double damage;
+
+        if (selectedAttack.isSpecial()) {
+            damage = ((selectedAttack.getAttackPower()) / (double) oponentPokemon.getDefenseSpecial()) + 2;
+        } else {
+            damage = ((selectedAttack.getAttackPower()) / (double) oponentPokemon.getDefenseFisic()) + 2;
+        }
+
+        oponentPokemon.receiveDamage((short)(damage*effectivity));
     }
 }
